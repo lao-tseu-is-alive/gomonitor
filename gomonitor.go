64a851454a22e220887040cbe1f0e54c6b42539d
Carlos GIL <lao.tseu.is.alive@gmail.com>
2020-10-27 14:17:34 +0100

    added url and filename parameter

--- gomonitor.go
+++ gomonitor.go
@@ -5,80 +5,78 @@ package main
 import (
 	"context"
 	"flag"
-	"io/ioutil"
-	"log"
-	"math"
-
 	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/device"
+	"github.com/lao-tseu-is-alive/golog"
+	"io/ioutil"
+	"log"
+	"math"
+	"time"
 )
+
+const VERSION = "0.2.0"
+const BuildDate = "27-10-2020"
 
 func main() {
 	const defaultUrl = "https://carto.lausanne.ch/"
+	const defaultFileName = "screenshot.jpg"
+	golog.Info("## STARTING GOMONITOR VERSION %s, BUILD AT %s", VERSION, BuildDate)
 	url2Monitor := flag.String("url", defaultUrl, "url you want to get screenshot")
+	filename := flag.String("filename", defaultFileName, "filename to save screenshot")
+	flag.Parse()
+	golog.Info("## will try url: %s, and save screenshot here: %s", *url2Monitor, *filename)
 	// create context
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 	// capture screenshot of an element
 	var buf []byte
-	if err := chromedp.Run(ctx, elementScreenshot(*url2Monitor, `#center`, &buf)); err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile("elementScreenshot.png", buf, 0644); err != nil {
-		log.Fatal(err)
-	}
 
 	// capture entire browser viewport, returning png with quality=90
 	if err := chromedp.Run(ctx, fullScreenshot(*url2Monitor, 90, &buf)); err != nil {
 		log.Fatal(err)
 	}
-	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0644); err != nil {
+	if err := ioutil.WriteFile(*filename, buf, 0644); err != nil {
 		log.Fatal(err)
 	}
 }
 
-// elementScreenshot takes a screenshot of a specific element.
-func elementScreenshot(url, sel string, res *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(url),
-		chromedp.WaitVisible(sel, chromedp.ByID),
-		chromedp.Screenshot(sel, res, chromedp.NodeVisible, chromedp.ByID),
-	}
-}
-
 // fullScreenshot takes a screenshot of the entire browser viewport.
-//
-// Liberally copied from puppeteer's source.
-//
-// Note: this will override the viewport emulation settings.
 func fullScreenshot(url string, quality int64, res *[]byte) chromedp.Tasks {
+	// The first task enables lifecycle events on the page and the second task navigates,
+	// and blocks until the desired event is received
 	return chromedp.Tasks{
 		enableLifeCycleEvents(),
-		//chromedp.Navigate(url),
+		chromedp.Emulate(device.IPad),
+		chromedp.EmulateViewport(1024, 768, chromedp.EmulateScale(2)),
+		// chromedp.Navigate(url),
 		navigateAndWaitFor(url, "networkIdle"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			// get layout metrics
 			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
 			if err != nil {
+				golog.Err("ERROR getting page.GetLayoutMetrics : %v", err)
 				return err
 			}
 
 			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
 
 			// force viewport emulation
-			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
+			err = emulation.SetDeviceMetricsOverride(int64(width), int64(height), 1, false).
 				WithScreenOrientation(&emulation.ScreenOrientation{
-					Type:  emulation.OrientationTypePortraitPrimary,
+					Type:  emulation.OrientationTypeLandscapePrimary,
 					Angle: 0,
 				}).
 				Do(ctx)
 			if err != nil {
+				golog.Err("ERROR doing  emulation.SetDeviceMetricsOverride : %v", err)
 				return err
 			}
 
 			// capture screenshot
 			*res, err = page.CaptureScreenshot().
+				WithFormat(page.CaptureScreenshotFormatJpeg).
 				WithQuality(quality).
 				WithClip(&page.Viewport{
 					X:      contentSize.X,
@@ -88,6 +86,7 @@ func fullScreenshot(url string, quality int64, res *[]byte) chromedp.Tasks {
 					Scale:  1,
 				}).Do(ctx)
 			if err != nil {
+				golog.Err("ERROR doing  page.CaptureScreenshot : %v", err)
 				return err
 			}
 			return nil
@@ -95,6 +94,7 @@ func fullScreenshot(url string, quality int64, res *[]byte) chromedp.Tasks {
 	}
 }
 
+// https://github.com/chromedp/chromedp/issues/431
 func enableLifeCycleEvents() chromedp.ActionFunc {
 	return func(ctx context.Context) error {
 		err := page.Enable().Do(ctx)
@@ -113,6 +113,7 @@ func navigateAndWaitFor(url string, eventName string) chromedp.ActionFunc {
 	return func(ctx context.Context) error {
 		_, _, _, err := page.Navigate(url).Do(ctx)
 		if err != nil {
+			golog.Err("Error doing page.Navigate : %v", err)
 			return err
 		}
 
@@ -133,7 +134,7 @@ func navigateAndWaitFor(url string, eventName string) chromedp.ActionFunc {
 // puppeteer implements this exactly.
 func waitFor(ctx context.Context, eventName string) error {
 	ch := make(chan struct{})
-	cctx, cancel := context.WithCancel(ctx)
+	cctx, cancel := context.WithTimeout(ctx, time.Duration(60)*time.Second) // let's have a 1m timeout
 	chromedp.ListenTarget(cctx, func(ev interface{}) {
 		switch e := ev.(type) {
 		case *page.EventLifecycleEvent:
